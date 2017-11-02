d1f62549e9cf7a6b7370853e8e864016c9dbea3b
Dmitriy Abramov <karvozavr@gmail.com>
2017-11-02 15:11:23 +0300

    Init

---
+++ CityQuest/app/src/main/java/ru/spbau/mit/karvozavr/cityquest/Quest.java
@@ -0,0 +1,13 @@
+package ru.spbau.mit.karvozavr.cityquest;
+
+/**
+ * Represents the quest entity
+ */
+public class Quest {
+
+    /**
+     * Contains all information about quest except the quest itself.
+     */
+    public static class QuestInfo {
+    }
+}
