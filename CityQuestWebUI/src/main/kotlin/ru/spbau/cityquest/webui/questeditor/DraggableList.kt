de5b08b6fdc087effdf01447c5347b5f69e68b06
Vasily Alferov <ya-ikmik2012@yandex.ru>
2017-12-27 01:31:27 +0300

    Drag & drop both forward & backeard (issue #13)

--- CityQuestWebUI/src/main/kotlin/ru/spbau/cityquest/webui/questeditor/DraggableList.kt
+++ CityQuestWebUI/src/main/kotlin/ru/spbau/cityquest/webui/questeditor/DraggableList.kt
@@ -1,6 +1,6 @@
 package ru.spbau.cityquest.webui.questeditor
 
-import org.w3c.dom.Node
+import org.w3c.dom.*
 import org.w3c.dom.events.Event
 
 class DraggableList {
@@ -16,12 +16,25 @@ class DraggableList {
 
     fun handleDragLeave(e: Event, elem: Node) { }
 
+    private fun nodeIndexInList(node : Node) : Int {
+        return (node.parentElement!! as HTMLUListElement).children.asList()
+                .indexOfFirst { listNode -> node.isSameNode(listNode) }
+    }
+
     fun handleDrop(e: Event, elem: Node) {
         e.stopPropagation()
 
         if (dragSource != elem) {
+            val i : Int = nodeIndexInList(elem)
+            val j : Int = nodeIndexInList(dragSource!!)
+            println("i=$i,j=$j")
             elem.parentNode?.removeChild(dragSource!!)
-            elem.parentNode?.insertBefore(dragSource!!, elem)
+            if (j > i) {
+                elem.parentNode?.insertBefore(dragSource!!, elem)
+            } else {
+                elem.parentNode?.insertBefore(dragSource!!,
+                        (elem.parentElement!! as HTMLUListElement).children[i + 1])
+            }
         }
     }
 }
