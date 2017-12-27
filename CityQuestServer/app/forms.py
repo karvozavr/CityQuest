380ca1a3a81aa361d2fb9b0d7427a9b3b5cfc883
subject-name-here <jura.paradov@gmail.com>
2017-12-28 00:22:00 +0300

    Final fixes. Now it pretends to work.

--- CityQuestServer/app/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import JsonQuest
-
-
-class InvisibleForm(forms.ModelForm):
-
-    class Meta:
-        model = JsonQuest
-        fields = ('json', )
--- CityQuestServer/app/models.py
+++ CityQuestServer/app/models.py
@@ -1,5 +1,1 @@
 from django.db import models
-
-
-class JsonQuest(models.Model):
-    json = models.TextField(max_length=100000)
