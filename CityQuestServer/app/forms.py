27b2c0ae5bef9938ddc5bbb29bee8032e20a43f6
subject-name-here <jura.paradov@gmail.com>
2017-12-27 23:45:44 +0300

    finally made it work

--- CityQuestServer/app/forms.py
+++ CityQuestServer/app/forms.py
@@ -1,9 +1,9 @@
 from django import forms
-from .models import json_quest
+from .models import JsonQuest
 
 
 class InvisibleForm(forms.ModelForm):
 
     class Meta:
-        model = json_quest
+        model = JsonQuest
         fields = ('json', )
