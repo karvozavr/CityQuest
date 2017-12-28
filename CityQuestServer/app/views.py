4d1e7c3a5f384aa131c444bb4dc927627128509c
subject-name-here <jura.paradov@gmail.com>
2017-12-28 10:40:19 +0300

    Almost integrated with web

--- CityQuestServer/app/views.py
+++ CityQuestServer/app/views.py
@@ -14,38 +14,41 @@ def index(request):
 
 
 def save(request):
-    json_data = request.POST.get('json')
+    #try:
+        json_data = request.POST.get('json')
 
-    deserialized = json.loads(json_data)
+        deserialized = json.loads(json_data)
 
-    q = QuestInfo()
-    q.name = deserialized["name"]
-    q.author = deserialized["author"]
-    q.image = '0'
-    q.avg_distance = deserialized["avg_distance"]
-    q.description = deserialized["description"]
+        q = QuestInfo()
+        q.name = deserialized["name"]
+        q.author = deserialized["author"]
+        q.image = '0'
+        q.avg_distance = deserialized["avg_distance"]
+        q.description = deserialized["description"]
 
-    q.save()
+        q.save()
 
-    for step in deserialized["steps"]:
+        for step in deserialized["steps"]:
+            s = QuestStep()
+            s.title = step["title"]
+            s.step_number = step["step_number"]
+            s.step_type = step["step_type"]
+            s.description = step["description"]
+            s.goal = step["goal"]
+            if s.step_type == 'geo':
+                s.latitude = step["latitude"]
+                s.longitude = step["longitude"]
+                s.keywords = ""
+            elif s.step_type == 'key':
+                s.keywords = step["keywords"]
 
-        s = QuestStep()
-        s.title = step["title"]
-        s.step_number = step["step_number"]
-        s.step_type = step["step_type"]
-        s.description = step["description"]
-        s.goal = step["goal"]
-        if s.step_type == 'geo':
-            s.latitude = step["latitude"]
-            s.longitude = step["longitude"]
-        elif s.step_type == 'key':
-            s.keywords = step["keywords"]
+            s.quest_host_id = q.id
+            s.save()
 
-        s.quest_host_id = q.id
 
-        s.save()
-
-    return HttpResponse(deserialized.items())
 
     # It is not clear if I should do it
-    # return redirect('index')
+    # return HttpResponse(deserialized.items())
+        #return redirect('index')
+    #except Exception:
+        return redirect('index')
