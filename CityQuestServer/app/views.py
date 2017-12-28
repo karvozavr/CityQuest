150f33bad9eb958f7d438ab78ad8602085279926
subject-name-here <jura.paradov@gmail.com>
2017-12-28 03:15:09 +0300

    Added loader from json. Soon I will wrap it all in try-catch.

--- CityQuestServer/app/views.py
+++ CityQuestServer/app/views.py
@@ -1,6 +1,10 @@
+import json
+
 from django.shortcuts import render, redirect
 from django.template import loader
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
+from data.models import QuestInfo, QuestStep
 
 
 def index(request):
@@ -10,9 +14,38 @@ def index(request):
 
 
 def save(request):
-    json = request.POST.get('json')
+    json_data = request.POST.get('json')
 
-    return HttpResponse(json)
+    deserialized = json.loads(json_data)
+
+    q = QuestInfo()
+    q.name = deserialized["name"]
+    q.author = deserialized["author"]
+    q.image = '0'
+    q.avg_distance = deserialized["avg_distance"]
+    q.description = deserialized["description"]
+
+    q.save()
+
+    for step in deserialized["steps"]:
+
+        s = QuestStep()
+        s.title = step["title"]
+        s.step_number = step["step_number"]
+        s.step_type = step["step_type"]
+        s.description = step["description"]
+        s.goal = step["goal"]
+        if s.step_type == 'geo':
+            s.latitude = step["latitude"]
+            s.longitude = step["longitude"]
+        elif s.step_type == 'key':
+            s.keywords = step["keywords"]
+
+        s.quest_host_id = q.id
+
+        s.save()
+
+    return HttpResponse(deserialized.items())
 
     # It is not clear if I should do it
     # return redirect('index')
